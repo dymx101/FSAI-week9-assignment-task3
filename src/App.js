1cf0e96c0c03c6b74f680ea704e262ada87ad165
Yiming Dong <dymx101@hotmail.com>
2025-06-08 11:46:20 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import GreetingComponent from "./GreetingComponent.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <GreetingComponent />
+    </div>
+  );
+}
---
+++ src/GreetingComponent.js
@@ -0,0 +1,10 @@
+function GreetingComponent() {
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      <p>This is a simple functional component.</p>
+    </div>
+  );
+}
+
+export default GreetingComponent;
